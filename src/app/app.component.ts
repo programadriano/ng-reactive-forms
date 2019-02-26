0e76c28680f615636f26705a99de7ab27e84056b
Thiago Adriano <thiago.adriano26@gmail.com>
2019-02-25 22:48:23 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Login } from "./models/login";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'ng-reactive-forms';
+export class AppComponent implements OnInit {
+  title = "ng-reactive-forms";
+  formCliente: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.createForm(new Login());
+  }
+
+  createForm(login: Login) {
+    this.formCliente = this.formBuilder.group({
+      email: [login.email],
+      password: [login.password],
+      remember: [login.remember]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formCliente.value);
+    this.formCliente.reset(new Login());
+  }
 }
---
+++ src/app/models/login.ts
@@ -0,0 +1,5 @@
+export class Login {
+  email: string;
+  password: number;
+  remember: boolean;
+}
